2684b1452b3d9377c341c4b8e9bfe6781ffaa4ee
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-12-19 13:22:14 -0800

    Add showing of info about the REBL JAR used

--- experiment-jdk8-01/start.sh
+++ experiment-jdk8-01/start.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
+USER_DEPS="$HOME/.clojure/deps.edn"
+REBL_JAR="$HOME/clj/rebl/REBL-LATEST.jar"
+
 set -x
 /bin/rm -fr .cpcache
 uname -a
-ls -l $HOME/.clojure/deps.edn
+ls -l "${USER_DEPS}"
 echo "----------------------------------------"
-cat $HOME/.clojure/deps.edn
+cat "${USER_DEPS}"
 echo "----------------------------------------"
+ls -l "${REBL_JAR}"
+openssl md5 "${REBL_JAR}"
 java -version
 clj -Sdescribe
 exec clj -R:rebl -m cognitect.rebl
